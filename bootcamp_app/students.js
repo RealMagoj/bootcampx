545f478cf49cc72d6b7a83484871023da05e3264
Dom Golac <domgolac@gmail.com>
2021-05-20 05:36:53 +0000

    update students.js, teachers.js to use parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,9 +13,9 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM cohorts
 JOIN students ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${argv[0]}%'
-LIMIT ${argv[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [`%${argv[0]}%`, argv[1]])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,9 +15,9 @@ pool.query(`
   JOIN teachers ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${argv[0]}'
+  WHERE cohorts.name = $1
   ORDER BY teacher;
-`)
+`, [argv[0]])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
