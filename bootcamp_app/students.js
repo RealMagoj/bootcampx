00c925335f78439955e64ca582dbdd48c66ad564
Dom Golac <domgolac@gmail.com>
2021-05-20 05:04:27 +0000

    Add pg module, create teachers.js, update students.js

---
+++ bootcamp_app/students.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const argv = process.argv.slice(2);
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM cohorts
+JOIN students ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${argv[0]}%'
+LIMIT ${argv[1]};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
